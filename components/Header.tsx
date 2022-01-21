dbefefba3fe69afdf958e45cc4c02f0dace4f7e7
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-01-21 21:38:59 +0530

    Header - Finished

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-5xl mx-auto">
         <div className="flex items-centre space-x-5">
             <Link href="/">
                 <img className="w-44 object-contain cursor-pointer" src=" https://links.papareact.com/yvf" alt="medium icon" />
@@ -15,7 +15,10 @@ function Header() {
             </div>
         </div>
 
-        <div></div>    
+        <div className="flex items-center space-x-5 text-green-600">
+            <h3 className=" text-green-600 ">Sign In</h3>
+            <h3 className="border rounded-full border-green-600 px-4 py-1 ">Get Started</h3>
+        </div>    
     </header>
 
     )
