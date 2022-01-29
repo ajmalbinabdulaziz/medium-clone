638ee6a649ea64c983051b04f7ce9cc9c26e6209
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-01-29 18:32:48 +0530

    frontend blog card completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ interface Props {
 export default function Home({ posts }: Props) {
   console.log(posts);
   return (
-    <div>
+    <div className='max-w-5xl mx-auto'>
       <Head>
         <title>Medium Clone</title>
         <link rel="icon" href="/favicon.ico" />
@@ -21,7 +21,7 @@ export default function Home({ posts }: Props) {
       <Header />
 
       <div className='flex justify-between items-center bg-yellow-400
-      border-y border-black py-10 lg:py-0 max-w-5xl mx-auto'>
+      border-y border-black py-10 lg:py-0'>
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl max-w-xl font-serif'>
             <span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect
@@ -32,18 +32,33 @@ export default function Home({ posts }: Props) {
           </h2>
         </div>
 
-        <div>
-          <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
-        </div>
+        <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+
       </div>
 
     {/* DISPLAYING POSTS */}
 
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3
+      md:gap-6 p-3 md:p-4'>
         {posts.map(post => (
           <Link key={post._id} href={`/posts/${post.slug.current}`} >
-            <div>
-              <img src={urlFor(post.mainImage).url()!} alt="" />
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+              <img className='h-64 w-full object-cover group-hover:scale-105
+              transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt="" />
+
+              <div className='flex justify-between p-5 bg-white'>
+                
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img className='w-12 h-12 rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+
+              </div>
+
             </div>
           </Link>
         ))}
